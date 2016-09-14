f5f525c453aaad471c5c6a61169fcd3273c4ddc0
harin123 <aparavasan@gmail.com>
2016-09-13 19:24:15 -0500

    Add files via upload

---
+++ WEB-INF/classes/com/bookstore/data/Book.java
@@ -0,0 +1,34 @@
+package com.bookstore.data;
+
+public class Book {
+	
+	private int id;
+	private String author;
+	private String title;
+	private float price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+}
